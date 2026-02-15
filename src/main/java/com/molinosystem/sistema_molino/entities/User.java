6e40ed10cd544cb5565d5db30972caa41fb46557
Gaspar09 <luisito.salas909@gmail.com>
2026-02-15 00:39:09 -0600

    created entity User

---
+++ src/main/java/com/molinosystem/sistema_molino/entities/User.java
@@ -0,0 +1,31 @@
+package com.molinosystem.sistema_molino.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@NoArgsConstructor
+public class User {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String userName;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String rol;
+
+    private boolean active;
+}
