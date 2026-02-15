0133fcbe439fa7dee3d012a26feaa3868d8d4b52
Gaspar09 <luisito.salas909@gmail.com>
2026-02-15 00:31:41 -0600

    creation of project

---
+++ src/main/java/com/molinosystem/sistema_molino/SistemaMolinoApplication.java
@@ -0,0 +1,13 @@
+package com.molinosystem.sistema_molino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaMolinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaMolinoApplication.class, args);
+	}
+
+}
