623513e63998563b500c852f5f7ef35283343e5c
Gaspar09 <luisito.salas909@gmail.com>
2026-02-20 11:48:24 -0600

    created entities rout settlement

---
+++ src/main/java/com/molinosystem/sistema_molino/entities/RoutSettlement.java
@@ -0,0 +1,39 @@
+package com.molinosystem.sistema_molino.entities;
+
+import java.math.BigDecimal;
+import java.security.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "rout_settlement")
+
+@Setter @Getter
+@NoArgsConstructor
+public class RoutSettlement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name = "delivery_id", nullable = false)
+    private User delivery;
+
+    @Column(name = "expected_cash")
+    private BigDecimal expectedCash;
+
+    @Column(name = "actual_cash")
+    private BigDecimal actualCash;
+
+    private BigDecimal difference;
+    
+    private Timestamp date;
+}
---
+++ src/main/java/com/molinosystem/sistema_molino/entities/RoutSettlementDetails.java
@@ -0,0 +1,43 @@
+package com.molinosystem.sistema_molino.entities;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "rout_settlement_details")
+
+@Setter @Getter
+@NoArgsConstructor
+public class RoutSettlementDetails {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long Id;
+
+    @JoinColumn(name = "rout_settlement_id", nullable = false)
+    private RoutSettlement routSettlement;
+
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(name = "quantity_out", nullable = false)
+    private BigDecimal quantityOut;
+
+    @Column(name = "quantity_return", nullable = true)
+    private BigDecimal quantityReturn;
+
+    @Column(name = "price_unit")
+    private BigDecimal priceUnit;
+
+    @Column(name = "price_total")
+    private BigDecimal priceTotal;
+}
