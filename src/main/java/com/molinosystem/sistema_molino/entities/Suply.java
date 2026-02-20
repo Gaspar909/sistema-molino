8a711a9767c73247ab650806b7ccbb0eb86f7d2a
Gaspar09 <luisito.salas909@gmail.com>
2026-02-19 22:37:11 -0600

    crate entitys suplies

---
+++ src/main/java/com/molinosystem/sistema_molino/entities/Suply.java
@@ -0,0 +1,28 @@
+package com.molinosystem.sistema_molino.entities;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "suplies")
+
+@Setter @Getter
+@NoArgsConstructor
+
+public class Suply {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private int stock;
+    private BigDecimal price;
+}
---
+++ src/main/java/com/molinosystem/sistema_molino/entities/SuplyUsage.java
@@ -0,0 +1,36 @@
+package com.molinosystem.sistema_molino.entities;
+
+import java.math.BigDecimal;
+import java.security.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "suply_usage")
+
+@Setter @Getter
+@NoArgsConstructor
+public class SuplyUsage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @ManyToOne
+    @JoinColumn(name = "suply_id", nullable = false)
+    private Suply suply;
+    
+    private BigDecimal amount;
+
+    @Column(name = "date_time")
+    private Timestamp dateTime;
+}
