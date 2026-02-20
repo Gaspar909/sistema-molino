2f91fd3888350ff15bafa8af4e4a9ec91f29e0f2
Gaspar09 <luisito.salas909@gmail.com>
2026-02-20 10:57:43 -0600

    created entity client

---
+++ src/main/java/com/molinosystem/sistema_molino/entities/Client.java
@@ -0,0 +1,33 @@
+package com.molinosystem.sistema_molino.entities;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "clients")
+
+@Setter @Getter
+@NoArgsConstructor
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String name;
+
+    private String address;
+
+    @Column(name = "credit_limit")
+    private BigDecimal creditLimit;
+    
+    private Boolean active;
+}
