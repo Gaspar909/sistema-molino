b7592e31e633e7d7c5989d75a266cfd3f0e6180e
Gaspar09 <luisito.salas909@gmail.com>
2026-04-12 22:22:03 -0600

    feature: finished userRepository

--- src/main/java/com/molinosystem/sistema_molino/repositories/UserRepository.java
+++ src/main/java/com/molinosystem/sistema_molino/repositories/UserRepository.java
@@ -4,6 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.molinosystem.sistema_molino.entities.User;
 
+import java.util.List;
+import java.util.Optional;
+
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+
+    Optional<User> findByUserName(String userName);
+    List<User> findUserFilter(String userName, String name, boolean active, String rol);
+    List<String>  findAllDistincRoles();
 }
