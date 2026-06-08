e5c8a96aeae3f357cfa0c003793d9e2c9357604d
Gaspar09 <luisito.salas909@gmail.com>
2026-06-07 20:47:39 -0600

    add functions at JwtService

--- src/main/java/com/molinosystem/sistema_molino/services/JwtService.java
+++ src/main/java/com/molinosystem/sistema_molino/services/JwtService.java
@@ -10,6 +10,7 @@ import javax.crypto.SecretKey;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
@@ -26,6 +27,14 @@ public class JwtService {
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
+    private Claims extractAllClaims (String token){
+        return Jwts.parser()
+        .verifyWith(getSinginKey())
+        .build()
+        .parseSignedClaims(token)
+        .getPayload();
+    }
+
     public String generateToken(String userName, String role){
         Map<String, Object> claims = new HashMap<>();
         claims.put("role", role);
@@ -37,5 +46,22 @@ public class JwtService {
         .expiration(new Date(System.currentTimeMillis() + this.expirationTime))
         .signWith(getSinginKey())
         .compact();
+    }
+
+    public String extractUserName(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public String extractRol(String token) {
+        return extractAllClaims(token).get("role", String.class);
+    }
+
+    public boolean isTokenExpired (String token) {
+        return extractAllClaims(token).getExpiration().before(new Date());
+    }
+
+    public boolean isTokenValid(String token, String userName) {
+        final String extractedUserName = extractUserName(token);
+        return (extractedUserName.equals(userName) && isTokenExpired(token));
     }
 }
