567e981731593907340d078f7100519a27f4ba3a
LuisSalas909 <114534585+Gaspar909@users.noreply.github.com>
2026-07-02 16:26:51 -0600

    Feature/unit flow (#6)
    * feature: add unitRepository
    
    * add unitdto
    
    * Add unit mapper
    
    * add unit service
    
    * add unit controller

---
+++ src/main/java/com/molinosystem/sistema_molino/controllers/UnitController.java
@@ -0,0 +1,75 @@
+package com.molinosystem.sistema_molino.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.molinosystem.sistema_molino.dtos.UnitDto;
+import com.molinosystem.sistema_molino.requests.UnitRequest;
+import com.molinosystem.sistema_molino.services.UnitService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+
+@RestController
+@RequestMapping("/api/unit")
+public class UnitController {
+    @Autowired
+    private UnitService unitService;
+
+    @GetMapping
+    public ResponseEntity<Page<UnitDto>> getUnits(
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int pageSize) {
+        
+        return ResponseEntity.ok(unitService.getAllUnit(page, pageSize));
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<UnitDto> getMethodName(@PathVariable Long id) {
+        return ResponseEntity.ok(unitService.getUnitById(id));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<Page<UnitDto>> getSearchUnit(
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int pageSize,
+        @RequestParam String search) {
+        return ResponseEntity.ok(unitService.searchUnit(search, page, pageSize));
+    }
+    
+    @PostMapping
+    public ResponseEntity<UnitDto> createUnit(@RequestBody UnitRequest newUnit) {
+        return ResponseEntity.status(200).body(unitService.createUnitDto(newUnit));
+    }
+    
+    @PutMapping
+    public ResponseEntity<UnitDto> updateUnit (@PathVariable Long id, @RequestBody UnitRequest upUnit) { 
+        return ResponseEntity.ok(unitService.updateUnit(id, upUnit));
+    }
+
+    @PutMapping("enable/{id}")
+    public ResponseEntity<UnitDto> enableUnit(@PathVariable Long id) {
+        return ResponseEntity.ok(unitService.enableUnit(id));
+    }
+
+    @DeleteMapping("disable/{id}")
+    public ResponseEntity<UnitDto> disableUnit (@PathVariable Long id){
+        return ResponseEntity.ok(unitService.disableUnit(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<UnitDto> deleteUnit (@PathVariable Long id){
+        return ResponseEntity.ok(unitService.deleteUnit(id));
+    }
+}
---
+++ src/main/java/com/molinosystem/sistema_molino/repositories/UnitRepository.java
@@ -0,0 +1,13 @@
+package com.molinosystem.sistema_molino.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import com.molinosystem.sistema_molino.entities.Unit;
+import java.util.List;
+
+
+public interface UnitRepository extends JpaRepository<Unit, Long>, JpaSpecificationExecutor<Unit> {
+    public List<Unit> findByName(String name); 
+    public List<Unit> findBySymbol(String symbol);
+}
