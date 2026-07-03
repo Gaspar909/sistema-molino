91724a8da42d80273c496231650f4358ccba2c56
Gaspar09 <luisito.salas909@gmail.com>
2026-07-02 18:28:16 -0600

    add product category repository

---
+++ src/main/java/com/molinosystem/sistema_molino/repositories/ProductCategoryRepository.java
@@ -0,0 +1,8 @@
+package com.molinosystem.sistema_molino.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface ProductCategoryRepository extends JpaRepository, JpaSpecificationExecutor{
+
+}
