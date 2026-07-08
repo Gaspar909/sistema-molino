e513315abee3b99d81f49e966cf1be6b0667a72b
Gaspar09 <luisito.salas909@gmail.com>
2026-07-07 21:10:59 -0600

    add page to product price repository

--- src/main/java/com/molinosystem/sistema_molino/repositories/ProductPriceRepository.java
+++ src/main/java/com/molinosystem/sistema_molino/repositories/ProductPriceRepository.java
@@ -1,12 +1,12 @@
 package com.molinosystem.sistema_molino.repositories;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.molinosystem.sistema_molino.entities.ProductPrice;
 
 public interface ProductPriceRepository extends JpaRepository<ProductPrice, Long> {
 
-    public List<ProductPrice> findByProductId(Long productId);
+    public Page<ProductPrice> findByProductId(Long productId, Pageable pageable);
 }
