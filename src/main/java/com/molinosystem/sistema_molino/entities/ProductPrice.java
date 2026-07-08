2700e81913bdbf2263f137fdbb75c41211d83f77
Gaspar09 <luisito.salas909@gmail.com>
2026-07-07 22:03:56 -0600

    add product price contrller

--- src/main/java/com/molinosystem/sistema_molino/entities/ProductPrice.java
+++ src/main/java/com/molinosystem/sistema_molino/entities/ProductPrice.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,7 +25,7 @@ public class ProductPrice {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
-    @OneToMany
+    @ManyToOne
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
     
