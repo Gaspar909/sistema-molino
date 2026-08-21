0efaa705ea08027b3b76288806b30df371ac197f
Gaspar09 <luisito.salas909@gmail.com>
2026-08-21 16:14:25 -0600

    add account movement repository

--- src/main/java/com/molinosystem/sistema_molino/repositories/AccountMovementRepository.java
+++ src/main/java/com/molinosystem/sistema_molino/repositories/AccountMovementRepository.java
@@ -1,13 +1,13 @@
 package com.molinosystem.sistema_molino.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import com.molinosystem.sistema_molino.entities.Account;
 import com.molinosystem.sistema_molino.entities.AccountMovement;
-import java.util.List;
 
 
 public interface AccountMovementRepository extends JpaRepository<AccountMovement, Long>, JpaSpecificationExecutor<AccountMovement>{
-    List<AccountMovement> findByAccount(Account account);
+    Page<AccountMovement> findByAccountId(Long id, Pageable pageable);
 }
